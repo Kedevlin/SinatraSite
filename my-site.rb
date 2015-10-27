d5f694d1ecc47ca5d4207f58744e039a30007cd3
Kelly <kelly_devlin@alumni.brown.edu>
2015-10-27 14:20:20 -0700

    Updated home page and added blog, about, and projects pages

--- my-site.rb
+++ my-site.rb
@@ -5,4 +5,13 @@ class MySite < Sinatra::Base
   get "/" do
     erb :index
   end
+  get "/about.html" do
+    erb :about
+  end
+  get "/blog.html" do
+    erb :blog
+  end
+  get "/projects.html" do
+    erb :projects
+  end
 end
