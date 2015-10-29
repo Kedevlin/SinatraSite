9174e2bc2634195ce4dd2517a8189f3656447931
Kelly <kelly_devlin@alumni.brown.edu>
2015-10-29 16:24:13 -0700

    added layout file, extracted titles to it

--- my-site.rb
+++ my-site.rb
@@ -3,19 +3,23 @@ require "sinatra"
 class MySite < Sinatra::Base
 
   get "/" do
+    @title = "Kelly's Website"
     erb :index
   end
 
   get "/about.html" do
+    @title = "About Kelly"
     erb :about
   end
 
   get "/blog.html" do
+    @title = "Kelly's Blog"
     erb :blog
   end
 
   get "/projects.html" do
+    @title = "Kelly's Projects"
     erb :projects
   end
-  
+
 end
